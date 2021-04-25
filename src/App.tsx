41ce1405e8924e8682264f15afa3d12c7e1541b1
wzhe@outlook.com <wzhe@outlook.com>
2021-04-25 20:01:49 +0800

    init...

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from './contexts/auth0-context';
 
-function App() {
+const App: React.FC = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  console.log(useAuth0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {
+        !isAuthenticated ?
+        <button onClick={loginWithRedirect}>login</button> : <button onClick={()=>logout()}>logout</button>
+        //!isAuthenticated ? <button>login</button> : <button>logout</button>
+      }
+    </main>
   );
 }
 
